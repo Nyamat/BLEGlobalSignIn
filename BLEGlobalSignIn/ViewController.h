f0e289285f37d403a3cd21d9d335cd0271b46994
Nyamat <admin@Admins-Mac-mini.local>
2015-07-09 10:49:15 +0530

    Initial Commit

---
+++ BLEGlobalSignIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLEGlobalSignIn
+//
+//  Created by Nyamat on 09/07/15.
+//  Copyright (c) 2015 SMART Cloud Computing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BLEGlobalSignIn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BLEGlobalSignIn
+//
+//  Created by Nyamat on 09/07/15.
+//  Copyright (c) 2015 SMART Cloud Computing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
